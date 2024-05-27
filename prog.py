b1e8ed8eb1651bd03444c53ef20d50ee9b3047f3
Marusi1a <marusiachasodiyka@gmail.com>
2024-05-27 16:47:19 +0300

    друге вікно

--- prog.py
+++ prog.py
@@ -9,7 +9,7 @@ class App:
         # Setting title
         root.title("Sea Battle")
         # Setting window size
-        width = 500
+        width = 900
         height = 500
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
@@ -30,18 +30,27 @@ class App:
             row_buttons = []
             for col in range(grid_size):
                 btn = tk.Button(root, text="0", command=lambda r=row, c=col: self.on_button_click(r, c))
-                btn.place(x=start_x + col * button_size, y=start_y + row * button_size, width=button_size,
+                btn.place(x=start_x * 1.5 + col * button_size, y=start_y + row * button_size, width=button_size,
+                          height=button_size)
+                row_buttons.append(btn)
+            self.buttons.append(row_buttons)
+
+        for row in range(grid_size):
+            row_buttons = []
+            for col in range(grid_size):
+                btn = tk.Button(root, text="0", command=lambda r=row, c=col: self.on_button_click(r, c))
+                btn.place(x=start_x * 0.5 + col * button_size, y=start_y + row * button_size, width=button_size,
                           height=button_size)
                 row_buttons.append(btn)
             self.buttons.append(row_buttons)
 
         start_btn1 = tk.Button(root, text="Почати гру", command=self.start_bnt_onclick1)
-        start_btn1.place(x=400, y=460, width=70, height=25)
+        start_btn1.place(x=700, y=460, width=70, height=25)
 
         start_btn = tk.Button(root, text="Почати", command=self.start_btn_onclick)
-        start_btn.place(x=30, y=460, width=70, height=25)
+        start_btn.place(x=450, y=460, width=70, height=25)
         stop_btn = tk.Button(root, text="Наступна дія", command=self.stop_btn_onclick)
-        stop_btn.place(x=200, y=460, width=100, height=25)
+        stop_btn.place(x=550, y=460, width=100, height=25)
 
         self.is_active = False
         self.actions = ["", "Розтавляємо корабель по 4", "Розтавляємо корабель по 3", "Розтавляємо корабелі по 2",
@@ -49,17 +58,28 @@ class App:
         self.current_action_index = 0
 
         self.lbl1 = tk.Label(root, text="Морський Бій")
-        self.lbl1.place(x=200, y=10)
+        self.lbl1.place(x=500, y=10)
 
         dictation = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j'}
         for col in range(1, 11):
             lbl1 = tk.Label(root, text=dictation[col])
-            lbl1.place(x=start_x + col * button_size - button_size, y=start_y - button_size, width=button_size,
+            lbl1.place(x=start_x* 1.5 + col * button_size - button_size, y=start_y - button_size, width=button_size,
                        height=button_size)
 
         for row in range(1, 11):
             lbl1 = tk.Label(root, text=str(row))
-            lbl1.place(x=start_x - button_size, y=start_y + row * button_size - button_size, width=button_size,
+            lbl1.place(x=start_x*1.5 - button_size, y=start_y + row * button_size - button_size, width=button_size,
+                       height=button_size)
+
+        dictation = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j'}
+        for col in range(1, 11):
+            lbl1 = tk.Label(root, text=dictation[col])
+            lbl1.place(x=start_x * 0.5 + col * button_size - button_size, y=start_y - button_size, width=button_size,
+                       height=button_size)
+
+        for row in range(1, 11):
+            lbl1 = tk.Label(root, text=str(row))
+            lbl1.place(x=start_x * 0.5 - button_size, y=start_y + row * button_size - button_size, width=button_size,
                        height=button_size)
 
     def right_side_ship(self, col, len_ship):
