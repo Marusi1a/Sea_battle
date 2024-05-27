36d1f77607036eb1df2a05991e3e626811465cb1
Nastyaknu <zimanastya@knu.ua>
2024-05-27 11:19:26 +0300

    Text in lb

--- prog.py
+++ prog.py
@@ -2,8 +2,10 @@ import tkinter as tk
 import tkinter.messagebox
 import random
 
-
+def on_button_click(row, col):
+    print(row, col)
 class App:
+
     def __init__(self, root):
         # Setting title
         root.title("Sea Battle")
@@ -17,7 +19,7 @@ class App:
         # root.resizable(width=False, height=False)
 
         grid_size = 10
-        max_size = 250  # Half the size of the window
+        max_size = 250
         button_size = max_size // grid_size
         start_x = (width - button_size * grid_size) // 2
         start_y = (height - button_size * grid_size) // 2
@@ -28,27 +30,29 @@ class App:
         for row in range(grid_size):
             row_buttons = []
             for col in range(grid_size):
-                btn = tk.Button(root, text="0", command=lambda r=row, c=col: self.grid_btn_onclick(r, c))
+                btn = tk.Button(root, text="0",command=lambda r=row,c= col:on_button_click(r,c))
                 btn.place(x=start_x + col * button_size, y=start_y + row * button_size, width=button_size,
                           height=button_size)
                 row_buttons.append(btn)
             self.buttons.append(row_buttons)
 
-        # Create the start and stop buttons
-        start_btn = tk.Button(root, text="Почати гру", command=self.start_btn_onclick)
-        start_btn.place(x=400, y=460, width=70, height=25)
+
+
+        start_btn1 = tk.Button(root, text="Почати гру", command=self.start_bnt_onclick1)
+        start_btn1.place(x=400, y=460, width=70, height=25)
 
         start_btn = tk.Button(root, text="Почати", command=self.start_btn_onclick)
         start_btn.place(x=30, y=460, width=70, height=25)
-
         stop_btn = tk.Button(root, text="Наступна дія", command=self.stop_btn_onclick)
         stop_btn.place(x=200, y=460, width=100, height=25)
 
         self.is_active = False
+        self.actions = ["", "Розтавляємо корабель по 4", "Розтавляємо корабель по 3", "Розтавляємо корабелі по 2"]
+        self.current_action_index = 0
 
-        ###
-        lbl1 = tk.Label(root, text="Морський Бій")
-        lbl1.place(x=200, y=10)
+        self.lbl1 = tk.Label(root, text="Морський Бій")
+        self.lbl1.place(x=200, y=10)
+
         dictation = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j'}
         for col in range(1, 11):
             lbl1 = tk.Label(root, text=dictation[col])
@@ -60,7 +64,7 @@ class App:
             lbl1.place(x=start_x - button_size, y=start_y + row * button_size - button_size, width=button_size,
                        height=button_size)
 
-    ###
+
     def grid_btn_onclick(self, row, col):
         btn = self.buttons[row][col]
         if btn["bg"] == "light blue":
@@ -69,25 +73,41 @@ class App:
         else:
             print(f"Button {row},{col} clicked")
 
+
     def start_btn_onclick(self):
         self.is_active = True
-        lbl1 = tk.Label(root, text="Ready? START!")
-        lbl1.place(x=200, y=10)
+        self.current_action_index = max(0, self.current_action_index - 1)
+        self.update_label(self.actions[self.current_action_index])
+       # lbl1 = tk.Label(root, text="Ready? START!")
+      #  lbl1.place(x=200, y=10)
 
-        # Вибір випадкової кнопки
+
+
+    def stop_btn_onclick(self):
+        if not self.is_active:
+            self.update_label("Спочатку натисніть кнопку 'Почати'")
+            return
+
+        self.current_action_index += 1
+        if self.current_action_index >= len(self.actions):
+            self.update_label("Всі кораблі розміщені")
+            return
+
+        self.update_label(self.actions[self.current_action_index])
+
+    def update_label(self, text):
+
+        self.lbl1.config(text=text)
+
+    def start_bnt_onclick1(self):
+        self.is_active = True
+
         random_row = random.randint(0, 9)
         random_col = random.randint(0, 9)
         random_button = self.buttons[random_row][random_col]
         random_button.config(bg='lightblue', text="■")
-
-    def stop_btn_onclick(self):
-        if self.is_active:
-            self.is_active = False
-            lbl1 = tk.Label(root, text="First, let's place the ship of 4 cells")
-            lbl1.place(x=200, y=10)
-        else:
-            lbl1 = tk.Label(root, text="Спочатку натисніть кнопку 'Почати'")
-            lbl1.place(x=200, y=10)
+        lbl1 = tk.Label(root, text="Гра почалась!")
+        lbl1.place(x=200, y=10)
 
 
 if __name__ == "__main__":
